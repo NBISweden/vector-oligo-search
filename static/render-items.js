630a45680a95beacad71d8f0399b23cf53fcfedb
Mattias Nyberg <mattias.nyberg@scilifelab.se>
2024-04-15 21:50:33 +0200

    refactor: move templates from script to html elements

--- static/render-items.js
+++ static/render-items.js
@@ -1,12 +1,15 @@
 function renderItems(items, pageSize, pageNumber, targetId) {
+  const annotationTemplate = getTemplate("annotation-template");
+  const rowTemplate = getTemplate("row-template");
+  const paginationItemTemplate = getTemplate("pagination-item-template");
+  const paginationTemplate = getTemplate("pagination-template")
+
   const pageCount = Math.ceil(items.length / pageSize);
   pageNumber = Math.min(pageCount - 1, pageNumber);
   const start = pageSize * pageNumber;
   const end = start + pageSize;
   const selectedItems = items.slice(start, end)
-  const annotationTemplate = (
-`<span class="annotated-sequence break-all" data-annotation="{{ label }}">{{ sequence }}</span>`
-  )
+  
   const processedItems = selectedItems.map(
     (item, index) => ({
       ...item,
@@ -26,56 +29,10 @@ function renderItems(items, pageSize, pageNumber, targetId) {
     })
   )
 
-  const rowTemplate = (
-`<div class="row my-lg-5 my-sm-2">
-  <h3>{{ gene_id }}</h3>
-  <div class="accordion" id="accordion-{{ index }}">
-    <div class="accordion-item">
-      <h4 class="accordion-header">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-annotated-{{ index }}" aria-expanded="true" aria-controls="collapse-annotated-{{ index }}">
-          Annotated sequence
-        </button>
-      </h4>
-      <div id="collapse-annotated-{{ index }}" class="accordion-collapse collapse show" data-bs-parent="#accordion-{{ index }}">
-        <div class="accordion-body">
-          <p>
-            {{{annotatedView}}}
-          </p>
-        </div>
-      </div>
-    </div>
-    <div class="accordion-item">
-      <h4 class="accordion-header">
-        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse-sequence-{{ index }}" aria-expanded="true" aria-controls="collapse-sequence-{{ index }}">
-          Sequence only
-        </button>
-      </h4>
-      <div id="collapse-sequence-{{ index }}" class="accordion-collapse collapse" data-bs-parent="#accordion-{{ index }}">
-        <div class="accordion-body">
-          <p>
-            <span class="annotated-sequence break-all" data-annotation="Oligo sequence">{{ sequence }}</span>
-          </p>
-        </div>
-      </div>
-    </div>
-  </div>
-</div>`
-  );
   const pageView = processedItems.map(item => Mustache.render(rowTemplate, item)).join("\n");
   const maxListedPages = 5;
   const pageGroupStart = Math.floor(pageNumber / maxListedPages) * maxListedPages;
   const listedPages = Math.min(pageCount, maxListedPages);
-  const paginationItemTemplate = (
-`<li class="page-item {{disabled}}"><a class="page-link {{active}}" href="{{href}}">{{label}}</a></li>`
-  );
-  const paginationTemplate = (
-`<nav aria-label="Page navigation" class="my-3">
-  <ul class="pagination justify-content-center">
-    {{{paginationItems}}}
-    <li class="page-item"><span class="pagination-label"> of {{pageCount}} pages.</span></li>
-  </ul>
-</nav>`
-  );
 
   function inPageRange(pn) {
     return pn >= 0 && pn < pageCount;
@@ -118,6 +75,14 @@ function renderItems(items, pageSize, pageNumber, targetId) {
     }
   )
   document.getElementById(targetId).innerHTML = [paginationView, pageView, paginationView].join("\n");
+}
+
+function getTemplate(templateId) {
+  try {
+    return document.getElementById(templateId).innerHTML;
+  } catch (e) {
+    throw new Error(`Unable to load template with id "${templateId}"`)
+  }
 }
 
 function updatePageState() {
